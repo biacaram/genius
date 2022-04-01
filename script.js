75633c0d3e790432c10164f8b38e15aa55c7eaaa
Beatriz Andre <biacaram@gmail.com>
2022-04-01 00:30:21 -0300

    seleciona cores e faz o check

---
+++ script.js
@@ -0,0 +1,51 @@
+let order = [];
+let clickOrder = [];
+let score = 0;
+
+// 0 - verde
+// 1 - vermelho
+// 2 - azul
+// 3 - amarelo
+
+const green = document.querySelector('.green');
+const red = document.querySelector('.red');
+const blue = document.querySelector('.blue');
+const yellow = document.querySelector('.yellow');
+
+// cria ordem aleatória
+let shuffleOrder = () => {
+    let color = Math.floor(Math.random() * 4);
+    order.push(color);
+    clickOrder = [];
+
+    for(let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+}
+
+// acende a próxima cor
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number - 250);
+
+    setTimeout(() => {
+        element.classList.remove('selected');
+    });
+}
+
+// checa se as cores foram clicadas na ordem certa
+let checkOrder = () => {
+    for(let i in clickOrder) {
+        if(clickOrder[i] != order[i]) {
+            lose();
+            break;
+        }
+    }
+    if(clickOrder.length == order.length) {
+        alert(`Pontuação: ${score}\n Você acertou! Iniciando próximo nível!`)
+        nextLevel();
+    }
+}
